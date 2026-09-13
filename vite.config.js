d61dabfac18d32fa5dda8dae2c25c4843c10133d
z213ghorbani-ux <z213ghorbani@gmail.com>
2026-09-13 14:02:54 +0330

    first commit

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,8 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://127.0.0.1:8000", // پورت واقعی بک‌اندتون رو اینجا بذارید
+        //target: "http://127.0.0.1:8000", // پورت واقعی بک‌اندتون رو اینجا بذارید
+        target: "http://188.121.114.194:9000"
         changeOrigin: true,
         secure: false,
       },
