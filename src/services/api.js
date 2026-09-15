76bcf0fef2c15dae94ba4b0085155560da022a4e
z213ghorbani-ux <z213ghorbani@gmail.com>
2026-09-15 09:51:38 +0330

    first commit

--- src/services/api.js
+++ src/services/api.js
@@ -1,31 +1,27 @@
-import axios from "axios";
+import api from "./api";
 
-const api = axios.create({
-  baseURL: "http://188.121.114.194:9000/api", // این دقیقاً وصل میشه به پروکسی vite.config
-  headers: {
-    Accept: "application/json",
-  },
-});
+export const getReports = async (params = {}) => {
+  const response = await api.get("/reports", {
+    params,
+  });
 
-// ارسال خودکار توکن در هر درخواست
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-});
+  return response.data;
+};
 
-// هندل خطای 401
-api.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (error.response && error.response.status === 401) {
-      localStorage.removeItem("token");
-      localStorage.removeItem("user");
-    }
-    return Promise.reject(error);
-  },
-);
+export const batchDeleteReports = async (fromDateOrObject, maybeToDate) => {
+  const payload =
+    typeof fromDateOrObject === "object" && fromDateOrObject !== null
+      ? {
+          from_date:
+            fromDateOrObject.from_date ?? fromDateOrObject.fromDate ?? "",
+          to_date: fromDateOrObject.to_date ?? fromDateOrObject.toDate ?? "",
+        }
+      : {
+          from_date: fromDateOrObject,
+          to_date: maybeToDate,
+        };
 
-export default api;
+  const response = await api.post("/reports/batch-delete", payload);
+
+  return response.data;
+};
